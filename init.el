d55293875c1cef7c12a73e9449f30b39a31153fd
shirry0129 <shimizy35@gmail.com>
2018-06-04 18:57:05 +0900

    今現在の一番最適な環境

---
+++ init.el
@@ -0,0 +1,175 @@
+;; 環境を日本語、UTF-8にする
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; 改行コードを表示する
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+;; 複数ウィンドウを禁止する
+(setq ns-pop-up-frames nil)
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+;; カーソル行をハイライトする
+(global-hl-line-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; スクリーンの最大化
+(set-frame-parameter nil 'fullscreen 'maximized)
+
+;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
+
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; beep音を消す
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
+
+;;c-h
+(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
+
+;;package
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; 折り返し
+(setq-default truncate-partial-width-windows t)
+(setq-default truncate-lines t)
+
+;;counsel
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters powerline badwolf-theme ace-window company counsel))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(ivy-mode 1) ;; デフォルトの入力補完がivyになる
+(counsel-mode 1)
+
+;;company
+(require 'company)
+(global-company-mode) ; 全バッファで有効にする 
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+(define-key company-active-map (kbd "M-n") nil)
+(define-key company-active-map (kbd "M-p") nil)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-h") nil)
+
+;; バックアップファイルを作らないようにする
+(setq make-backup-files nil)
+;;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;;badwolf
+(load-theme 'badwolf t)
+
+;;; スクロールを一行ずつにする
+(setq scroll-conservatively 1)
+
+;;; モードラインに情報を表示
+(display-time)
+(line-number-mode 1)
+(column-number-mode 1)
+
+;;; 現在の関数名をモードラインに表示
+(which-function-mode 1)
+
+;;マウス無効化
+(global-unset-key [mouse-1])
+(global-unset-key [down-mouse-1])
+(global-unset-key [drag-mouse-1])
+(global-unset-key [double-mouse-1])
+(global-unset-key [double-drag-Mouse-1])
+(global-unset-key [triple-mouse-1])
+(global-unset-key [triple-drag-mouse-1])
+(global-unset-key [\S-down-mouse-1])
+(global-unset-key [\C-down-mouse-1])
+(global-unset-key [\M-mouse-1])
+(global-unset-key [\M-down-mouse-1])
+(global-unset-key [\M-drag-mouse-1])
+(global-unset-key [mouse-2])
+(global-unset-key [mouse-3])
+(global-unset-key [\S-mouse-3])
+(global-unset-key [\S-down-mouse-3])
+(global-unset-key [\C-down-mouse-3])
+(global-unset-key [\M-mouse-3])
+
+;; Ricty フォントの利用
+(create-fontset-from-ascii-font "Ricty-14:weight=normal:slant=normal" nil "ricty")
+(set-fontset-font "fontset-ricty"
+                  'unicode
+                  (font-spec :family "Ricty" :size 14)
+                  nil
+                  'append)
+(add-to-list 'default-frame-alist '(font . "fontset-ricty"))
+
+;;powerline
+(require 'powerline)
+(powerline-default-theme)
+
+;;rainbow
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
